7d61934f00e89918baf0dce8af4a899fa197d7d9
eugeneyan <eugeneyanziyou@gmail.com>
2016-11-22 11:34:29 +0800

    Update sample_tunevgg1 to use our own model

--- image/sample_tunevgg1.py
+++ image/sample_tunevgg1.py
@@ -8,6 +8,8 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.layers import Activation, Dropout, Flatten, Dense
+from dl_models.vgg16 import VGG16
+
 
 # path to the model weights file.
 weights_path = 'data/images_clothes/model/vgg16_weights.h5'
@@ -25,60 +27,63 @@ nb_epoch = 20
 def save_bottlebeck_features():
     datagen = ImageDataGenerator(rescale=1./255)
 
-    # build the VGG16 network
-    model = Sequential()
-    model.add(ZeroPadding2D((1, 1), input_shape=(3, img_width, img_height)))
-
-    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    # load the weights of the VGG16 networks
-    # (trained on ImageNet, won the ILSVRC competition in 2014)
-    # note: when there is a complete match between your model definition
-    # and your weight savefile, you can simply call model.load_weights(filename)
-    assert os.path.exists(weights_path), 'Model weights not found (see "weights_path" variable in script).'
-    f = h5py.File(weights_path)
-    for k in range(f.attrs['nb_layers']):
-        if k >= len(model.layers):
-            # we don't look at the last (fully-connected) layers in the savefile
-            break
-        g = f['layer_{}'.format(k)]
-        weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
-        model.layers[k].set_weights(weights)
-    f.close()
+    # # build the VGG16 network
+    # model = Sequential()
+    # model.add(ZeroPadding2D((1, 1), input_shape=(3, img_width, img_height)))
+    #
+    # model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
+    # model.add(ZeroPadding2D((1, 1)))
+    # model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
+    # model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    #
+    # model.add(ZeroPadding2D((1, 1)))
+    # model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
+    # model.add(ZeroPadding2D((1, 1)))
+    # model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
+    # model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    #
+    # model.add(ZeroPadding2D((1, 1)))
+    # model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
+    # model.add(ZeroPadding2D((1, 1)))
+    # model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
+    # model.add(ZeroPadding2D((1, 1)))
+    # model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
+    # model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    #
+    # model.add(ZeroPadding2D((1, 1)))
+    # model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
+    # model.add(ZeroPadding2D((1, 1)))
+    # model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
+    # model.add(ZeroPadding2D((1, 1)))
+    # model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
+    # model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    #
+    # model.add(ZeroPadding2D((1, 1)))
+    # model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
+    # model.add(ZeroPadding2D((1, 1)))
+    # model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
+    # model.add(ZeroPadding2D((1, 1)))
+    # model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
+    # model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    #
+    # # load the weights of the VGG16 networks
+    # # (trained on ImageNet, won the ILSVRC competition in 2014)
+    # # note: when there is a complete match between your model definition
+    # # and your weight savefile, you can simply call model.load_weights(filename)
+    # assert os.path.exists(weights_path), 'Model weights not found (see "weights_path" variable in script).'
+    # f = h5py.File(weights_path)
+    # for k in range(f.attrs['nb_layers']):
+    #     if k >= len(model.layers):
+    #         # we don't look at the last (fully-connected) layers in the savefile
+    #         break
+    #     g = f['layer_{}'.format(k)]
+    #     weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
+    #     model.layers[k].set_weights(weights)
+    # f.close()
+    model = VGG16(include_top=False, weights='imagenet', input_tensor=None)
     print('Model loaded.')
+
+    datagen = ImageDataGenerator(rescale=1./255)
 
     generator = datagen.flow_from_directory(
         train_data_dir,
