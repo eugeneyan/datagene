c869d0097262db00bb5b3a59d568582b4a855d2b
eugeneyan <eugeneyanziyou@gmail.com>
2016-12-21 16:05:03 +0800

    Update to use uwsgi

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+"""
+uwsgi file
+"""
+from app.routes import app # uwsgi expects a variable called application
+# http://stackoverflow.com/questions/12030809/flask-and-uwsgi-unable-to-load-app-0-mountpoint-callable-not-found-or-im
+# To get around this, in uwsgi.ini we set callable = app
+
+
+if __name__ == '__main__':
+    app.run()
