05f94fcd6698b9039675ad8dc728d973ece30bf8
eugeneyan <eugeneyanziyou@gmail.com>
2016-12-30 19:07:42 +0800

    Add AWS AMI static folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-
 # Initialize app
 app = Flask(__name__, static_folder='/Users/eugeneyan/eugeneyan/datagene/', static_url_path='')
-# app = Flask(__name__, static_folder='/Users/eugeneyan/eugeneyan/datagene/', static_url_path='')
+# app = Flask(__name__, static_folder='/home/ubuntu/datagene/', static_url_path='')
