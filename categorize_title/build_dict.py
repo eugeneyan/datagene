bcd01b740634a4309ff09bae511fb95626d9cae9
eugeneyan <eugeneyanziyou@gmail.com>
2016-12-30 16:23:45 +0800

    Refactored category to categorize_title

---
+++ categorize_title/build_dict.py
@@ -0,0 +1,243 @@
+"""
+Builds model to categorize product based on title. Steps include:
+- Loading title and category data
+- Cleaning title and category data (based on data_prep.clean titles)
+- Creates and save dictionary model in pickle format
+
+python -m categorize.create_dict title_category_keep_samp_small categorization_dicts
+python -m categorize.create_dict title_category_keep categorization_dicts
+"""
+import math
+import os
+import sys
+import cPickle as pickle
+from collections import defaultdict
+from data_prep.clean_titles import load_data, remove_no_category, encode_title, lowercase_title, tokenize_title, \
+    remove_stopwords, remove_numeric, remove_one_char_words, remove_empty_titles, STOP_WORDS
+from utils.logger import logger
+
+
+def create_mapping_dicts(df, category='regional_key'):
+    """ (df, str) -> dict, dict
+
+    Returns two dictionaries mapping category paths to integers, and mapping integers to category_paths.
+
+    :param df:
+    :param category:
+    """
+
+    # Get a array of unique categories
+    category_paths = df[category].unique()
+
+    # Sort category_paths
+    category_paths.sort()
+
+    # Create category_to_int_dict and int to category_dict
+    category_to_int_dict = {key: value for key, value in zip(category_paths, range(len(category_paths)))}
+    int_to_category_dict = {value: key for key, value in category_to_int_dict.items()}
+
+    return category_to_int_dict, int_to_category_dict
+
+
+def convert_category_to_int(df, category_to_int_dict, category='regional_key', ):
+    """
+
+    Returns a dataframe where the categories (str) have been converted to int.
+
+    :param df:
+    :param category:
+    :param category_to_int_dict:
+    :return:
+    """
+    df[category] = df[category].apply(lambda x: category_to_int_dict[x])
+    return df
+
+
+def find_ngrams(input_list, n):
+    """ list, int -> list(tuples)
+
+    Return a list of ngram tuples, where each tuple contains n unigrams.
+
+    :param input_list:
+    :param n:
+    :return:
+    >>> find_ngrams(['A', 'B', 'C', 'D'], 2)
+    [('A', 'B'), ('B', 'C'), ('C', 'D')]
+    >>> find_ngrams(['A', 'B', 'C', 'D'], 3)
+    [('A', 'B', 'C'), ('B', 'C', 'D')]
+    """
+    return zip(*[input_list[i:] for i in range(n)])
+
+
+def create_ngram_from_tokens(tokens):
+    """ list(str) -> list(str)
+
+    Returns a list of ngram strings from a list of ngram tokens.
+
+    :param tokens:
+    :return:
+    >>> create_ngram_from_tokens(['A', 'B', 'C', 'D'])
+    ['A', 'B', 'C', 'D', 'A_B', 'B_C', 'C_D', 'A_B_C', 'B_C_D']
+    """
+
+    bigram_list = find_ngrams(tokens, 2)
+    trigram_list = find_ngrams(tokens, 3)
+
+    bigrams = [tuple[0] + '_' + tuple[1] for tuple in bigram_list]
+    trigrams = [tuple[0] + '_' + tuple[1] + '_' + tuple[2] for tuple in trigram_list]
+
+    ngram_list = tokens + bigrams + trigrams
+    return ngram_list
+
+
+def create_ngram(df, title='title'):
+    """ (DataFrame) -> DataFrame
+
+    Returns a DataFrame where the title is converted from str to ngrams.
+
+    :param df:
+    :param title:
+    :return:
+    """
+
+    df[title] = df[title].apply(create_ngram_from_tokens)
+    logger.info('{}: ngrams created'.format(title))
+    return df
+
+
+def create_tfidf_dict(train, title='title', category='regional_key'):
+    """ (DataFrame, str, str) -> defaultdict
+
+    Returns a tf-idf dict given a dataframe containing title and regional_key.
+
+    :param train:
+    :param title:
+    :param category:
+    :return:
+    """
+
+    # Create tf dictionary (though the name is tfidf, it's only tf for now)
+    ngram_dict_tfidf = defaultdict()
+
+    # For each token in the titles, create a dict as its value
+    for i, row in train.iterrows():
+        tokens = row[title]
+        for token in tokens:
+            ngram_dict_tfidf[token] = defaultdict()
+
+    logger.info('TF dict phase 1 done')
+
+    # For each token in the titles, add the token frequency to the value of the token key
+    # Token frequency = token count / total number of tokens in title
+    for i, row in train.iterrows():
+        tokens = row[title]
+        regional_id = row[category]
+        for token in tokens:
+            token_tf = tokens.count(token) / float(len(tokens))
+            try:
+                ngram_dict_tfidf[token][regional_id] += token_tf
+            except KeyError:
+                ngram_dict_tfidf[token][regional_id] = token_tf
+
+    logger.info('TF dict phase 2 done')
+
+    # create idf dictionary and count the number of titles in train
+    ngram_dict_idf = defaultdict()
+    no_of_skus = len(train)
+
+    # For each token in the title, add one to the value of the token key
+    for i, row in train.iterrows():
+        tokens = set(row[title])
+        for token in tokens:
+            try:
+                ngram_dict_idf[token] += 1
+            except KeyError:
+                ngram_dict_idf[token] = 1
+
+    # For each token in idf dict, divide the total number of skus (logged) by the count of token value
+    # Add 1 to the numerator to prevent zero divison error
+    for term, count in ngram_dict_idf.iteritems():
+        ngram_dict_idf[term] = math.log(no_of_skus) / float(1 + count)
+
+    logger.info('IDF dict done')
+
+    # Multiple values in tf dictionary with idf dictionary to get tf-idf dictionary
+    for ngram, cat_dict in ngram_dict_tfidf.iteritems():
+        # print ngram
+        ngram_idf = ngram_dict_idf[ngram]
+        # print ngram_idf
+        for regional_key, count in cat_dict.iteritems():
+            ngram_dict_tfidf[ngram][regional_key] = count * ngram_idf
+
+    logger.info('TF-IDF dict done')
+    return ngram_dict_tfidf
+
+
+def save_dict(tfidf_dict, int_to_category_dict, output_dir, output_name):
+    """ (defaultdict, dict, str, str) -> NoneType
+
+    Saves the dictionaries (tfidf_dict, int_to_category_dict) into pickle format.
+
+    :param tfidf_dict:
+    :param int_to_category_dict:
+    :param output_dir:
+    :param output_name:
+    :return:
+    """
+    output_path = os.path.join(output_dir, output_name + '.pickle')
+
+    with open(output_path, 'wb') as handle:
+        pickle.dump((tfidf_dict, int_to_category_dict), handle, protocol=2)
+        logger.info('Dict saved in {}'.format(output_path))
+
+
+if __name__ == '__main__':
+    data_dir = 'data/output'
+    input_file = sys.argv[1]
+    output_dir = 'data/model'
+    output_dict_name = sys.argv[2]
+    input_path = os.path.join(data_dir, input_file + '.csv')
+    title_col = 'title'
+    category_col = 'category_path'
+
+    # Load data
+    df = load_data(input_path, sku_id='asin')
+
+    # Remove records with no category
+    df = remove_no_category(df, category=category_col)
+
+    # Create mapping dicts
+    category_to_int_dict, int_to_category_dict = create_mapping_dicts(df, category='category_path')
+
+    # Convert categories to integers
+    df = convert_category_to_int(df, category_to_int_dict, category='category_path')
+
+    # Encode title as ascii
+    df = encode_title(df, title='title')
+
+    # Lowercase title
+    df = lowercase_title(df, title='title')
+
+    # Tokenize title
+    df = tokenize_title(df, title='title', excluded='-.')
+
+    # Remove stopwords (includes spam, colours)
+    df = remove_stopwords(df, stopwords=STOP_WORDS, title='title')
+
+    # Remove words that are solely numeric
+    df = remove_numeric(df, title='title')
+
+    # Remove words with character length == 1
+    df = remove_one_char_words(df, word_len=1, title='title')
+
+    # Remove titles that are empty
+    df = remove_empty_titles(df, title='title')
+
+    # Create ngrams for titles
+    df = create_ngram(df, title='title')
+
+    # Create tf-idf model dictionary
+    tfidf_dict = create_tfidf_dict(df, title='title', category='category_path')
+
+    # Save dict to pickle file
+    save_dict(tfidf_dict, int_to_category_dict, output_dir, output_dict_name)
---
+++ categorize_title/categorize_batch.py
@@ -0,0 +1,117 @@
+"""
+Batch categorize products given a csv. Will provide three category options.
+
+python -m categorize.categorize_batch 2016-07-04 TH
+"""
+import sys
+import os
+from data_prep.clean_titles import load_data, encode_title, lowercase_title, tokenize_title, \
+    remove_stopwords, remove_numeric, remove_one_char_words, STOP_WORDS, HTML_PARSER
+from categorize_title.build_dict import create_ngram
+from categorize_title.categorize_utils import load_dict, get_score, get_top_n_score
+from utils.create_dir import create_dir
+from utils.logger import logger
+
+
+def create_options(df, title, tfidf_dict):
+    """
+
+    Returns a dataframe with top three category options.
+
+    :param df:
+    :param title:
+    :param tfidf_dict:
+    :return:
+    """
+    df['options'] = df[title].apply(get_score, args=(tfidf_dict, 3,))
+    df['option1'] = df['options'].apply(get_top_n_score, args=(1,))
+    df['option2'] = df['options'].apply(get_top_n_score, args=(2,))
+    df['option3'] = df['options'].apply(get_top_n_score, args=(3,))
+    logger.info('Top 3 options created')
+    return df
+
+
+def save_results(df, output_dir, country):
+    """
+
+    Save recategorized result to csv.
+
+    :param df:
+    :param output_dir:
+    :param country:
+    :return:
+    """
+
+    # create recategorized directory path
+    recategorized_file = os.path.basename('product_{}_recategorized.csv'.format(country))
+    recategorized_file_path = os.path.join(output_dir, recategorized_file)
+
+    # Create output directory
+    create_dir(output_dir)
+
+    # Drop unnecessary columns
+    df.drop(labels=['title', 'options'], axis=1, inplace=True)
+
+    # Save to csv
+    df.to_csv(recategorized_file_path, index=False)
+    logger.info('Recategorized file save to csv here: {}'.format(output_dir))
+
+
+if __name__ == '__main__':
+    date = sys.argv[1]
+    country = sys.argv[2]
+    input_path = os.path.join('data/{}/product'.format(date), 'product_{}'.format(country) + '.csv')
+    output_dir = os.path.join('data/{}/product/recategorized'.format(date))
+    title_col = 'title'
+    category_col = 'regional_key'
+
+    # Load csv file
+    df = load_data(input_path)
+    logger.info('Data loaded')
+    logger.debug(df.head())
+
+    # Encode title as ascii
+    df = encode_title(df, title='title', parser=HTML_PARSER)
+    logger.info('Title encoded')
+    logger.debug(df.head())
+
+    # Lowercase title
+    df = lowercase_title(df, title='title')
+    logger.info('Title lowercased')
+    logger.debug(df.head())
+
+    # Tokenize title
+    df = tokenize_title(df, title='title', excluded="-.'")
+    logger.info('Title tokenize')
+
+    # Remove stopwords (includes spam, colours)
+    df = remove_stopwords(df, stopwords=STOP_WORDS, title='title')
+    logger.info('Title stopwords removed')
+    logger.debug(df.head())
+
+    # Remove words that are solely numeric
+    df = remove_numeric(df, title='title')
+    logger.info('Title numerics removed')
+    logger.debug(df.head())
+
+    # Remove words with character length == 1
+    df = remove_one_char_words(df, word_len=1, title='title')
+    logger.info('Title one char words removed')
+    logger.debug(df.head())
+
+    # Create ngrams for titles
+    df = create_ngram(df, title='title')
+    logger.info('Title ngrams created')
+    logger.debug(df.head())
+
+    # Load dictionary
+    tfidf_dict, regional_to_path_dict = load_dict('data/{}/model'.format(date),
+                                                  'categorization_dict_{}'.format(country))
+    logger.info('Dictionary loaded')
+
+    # Create options
+    df = create_options(df, title='title', tfidf_dict=tfidf_dict)
+    logger.info('Category options created')
+
+    # Save to csv
+    save_results(df, output_dir, country)
